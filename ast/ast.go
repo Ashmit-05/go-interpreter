adf26df458d018f99aed8f4c574bcbb1c37b1e92
Ashmit05 <ashmitbhardwaj05@gmail.com>
2024-02-22 23:32:05 +0530

    working on parsers

--- ast/ast.go
+++ ast/ast.go
@@ -1,27 +1,56 @@
 package ast
 
+import "github.com/Ashmit-05/go-interpreter/token"
+
 type Node interface {
-  TokenLiteral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
-  Node
-  statementNode()
+	Node
+	statementNode()
 }
 
 type Expression interface {
-  Node
-  expressionNode()
+	Node
+	expressionNode()
 }
 
 type Program struct {
-  Statements []Statement
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
 }
 
 func (p *Program) TokenLiteral() string {
-  if len(p.Statements) > 0 {
-    return p.Statements[0].TokenLiteral()
-  } else {
-    return ""
-  }
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
