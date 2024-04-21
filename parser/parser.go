ab99cb8cdc56b1e99c4d5f0bb1a0dd93ca40f84c
ashmit <ashmitbhardwaj05@gmail.com>
2024-04-21 22:48:21 +0530

    parsing integer literals

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Ashmit-05/go-interpreter/ast"
 	"github.com/Ashmit-05/go-interpreter/lexer"
@@ -43,6 +44,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// Read two tokens so that both current and next token are set
 	p.nextToken()
@@ -153,6 +155,21 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token : p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal,0,64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer",p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
 
 // helper functions
